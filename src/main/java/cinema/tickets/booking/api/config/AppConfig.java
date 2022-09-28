b574587dfc15aa13f73edcf4d5e5a868cee9ed20
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-28 20:49:02 +0300

    added app and dispatcher servlet configs

---
+++ src/main/java/cinema/tickets/booking/api/config/AppConfig.java
@@ -0,0 +1,59 @@
+package cinema.tickets.booking.api.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = "cinema.tickets.booking.api")
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/movie_booking");
+            dataSource.setUser("root");
+            dataSource.setPassword("root");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("cinema.tickets.booking.api.entity");
+
+        Properties hibernateProperty = new Properties();
+        hibernateProperty.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        hibernateProperty.setProperty("hibernate.show_sql", "true");
+
+        sessionFactory.setHibernateProperties(hibernateProperty);
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+
+        return transactionManager;
+    }
+}
