6da7c7642e3ae4e9afbf5c903ee08442e4c7edae
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-28 21:41:36 +0300

    added movie entity

---
+++ src/main/java/cinema/tickets/booking/api/entity/Movie.java
@@ -0,0 +1,68 @@
+package cinema.tickets.booking.api.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "duration_min")
+    private int durationMin;
+
+    @Column(name = "genre")
+    private String genre;
+
+    @Column(name = "director")
+    private String director;
+
+    public Movie() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDurationMin() {
+        return durationMin;
+    }
+
+    public void setDurationMin(int durationMin) {
+        this.durationMin = durationMin;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+}
