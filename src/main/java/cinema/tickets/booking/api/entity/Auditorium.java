4a43f314617de148ff0834610c02e66f44ce2339
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-28 21:11:57 +0300

    added auditorium entity

---
+++ src/main/java/cinema/tickets/booking/api/entity/Auditorium.java
@@ -0,0 +1,46 @@
+package cinema.tickets.booking.api.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "auditorium")
+public class Auditorium {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "seats_count")
+    private int seatsCount;
+
+    public Auditorium() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeatsCount() {
+        return seatsCount;
+    }
+
+    public void setSeatsCount(int seatsCount) {
+        this.seatsCount = seatsCount;
+    }
+}
