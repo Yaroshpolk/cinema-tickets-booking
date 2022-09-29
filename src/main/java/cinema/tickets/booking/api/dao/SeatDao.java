fb85057a3449a8d2be2f7c5701d915149dfcc74a
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-29 20:02:55 +0300

    added repostitory annotation

--- src/main/java/cinema/tickets/booking/api/dao/SeatDao.java
+++ src/main/java/cinema/tickets/booking/api/dao/SeatDao.java
@@ -4,9 +4,11 @@ import cinema.tickets.booking.api.entity.Seat;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class SeatDao implements Dao<Seat> {
 
     @Autowired
