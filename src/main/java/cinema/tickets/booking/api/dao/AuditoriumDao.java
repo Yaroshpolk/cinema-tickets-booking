907d6eca4ff19b88ec89b14135b38c021ac660e2
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-29 17:18:43 +0300

    added auditorium DAO

---
+++ src/main/java/cinema/tickets/booking/api/dao/AuditoriumDao.java
@@ -0,0 +1,44 @@
+package cinema.tickets.booking.api.dao;
+
+import cinema.tickets.booking.api.entity.Auditorium;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class AuditoriumDao implements Dao<Auditorium>{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Auditorium> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("from Auditorium", Auditorium.class).getResultList();
+    }
+
+    @Override
+    public Auditorium getById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.get(Auditorium.class, id);
+    }
+
+    @Override
+    public void save(Auditorium auditorium) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.saveOrUpdate(auditorium);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.createQuery("delete from Auditorium where id=:id").setParameter("id",id).executeUpdate();
+    }
+}
