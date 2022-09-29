c2c99a2db70b592296664f407bb5098484194307
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-29 20:54:25 +0300

    added reservation DAO

---
+++ src/main/java/cinema/tickets/booking/api/dao/ReservationDao.java
@@ -0,0 +1,44 @@
+package cinema.tickets.booking.api.dao;
+
+import cinema.tickets.booking.api.entity.Reservation;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ReservationDao implements Dao<Reservation> {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Reservation> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("from Reservation", Reservation.class).getResultList();
+    }
+
+    @Override
+    public Reservation getById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.get(Reservation.class, id);
+    }
+
+    @Override
+    public void save(Reservation reservation) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.saveOrUpdate(reservation);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.createQuery("delete from Reservation where id=:id").setParameter("id", id).executeUpdate();
+    }
+}
