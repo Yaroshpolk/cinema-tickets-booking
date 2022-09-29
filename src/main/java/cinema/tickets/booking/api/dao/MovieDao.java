0343508b21453f16c45b0fb2ceb5786e9c076d02
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-29 18:17:11 +0300

    added movie DAO

---
+++ src/main/java/cinema/tickets/booking/api/dao/MovieDao.java
@@ -0,0 +1,45 @@
+package cinema.tickets.booking.api.dao;
+
+import cinema.tickets.booking.api.entity.Auditorium;
+import cinema.tickets.booking.api.entity.Movie;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MovieDao implements Dao<Movie>{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Movie> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.createQuery("from Movie", Movie.class).getResultList();
+    }
+
+    @Override
+    public Movie getById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        return session.get(Movie.class, id);
+    }
+
+    @Override
+    public void save(Movie movie) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.saveOrUpdate(movie);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Session session = sessionFactory.getCurrentSession();
+
+        session.createQuery("delete from Movie where id=:id").setParameter("id",id).executeUpdate();
+    }
+}
