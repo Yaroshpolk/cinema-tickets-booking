a6f85463a5ec783f1792a8c1aa32ad840ca7664b
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-29 20:50:59 +0300

    added reservation entity

---
+++ src/main/java/cinema/tickets/booking/api/entity/Reservation.java
@@ -0,0 +1,57 @@
+package cinema.tickets.booking.api.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reservation")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @OneToOne
+    private User user;
+
+    @OneToOne
+    private Screening screening;
+
+    @OneToOne
+    private Seat seat;
+
+    public Reservation() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Screening getScreening() {
+        return screening;
+    }
+
+    public void setScreening(Screening screening) {
+        this.screening = screening;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+}
