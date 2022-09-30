801de1b644f4ff7e03d83f10b6462f810a53ff34
Yaroshevich Vladislav <yaroshevich00@yandex.ru>
2022-09-30 15:10:53 +0300

    added movie controller

---
+++ src/main/java/cinema/tickets/booking/api/controller/MovieController.java
@@ -0,0 +1,50 @@
+package cinema.tickets.booking.api.controller;
+
+import cinema.tickets.booking.api.entity.Movie;
+import cinema.tickets.booking.api.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+
+    @GetMapping("/")
+    public List<Movie> getAllMovies() {
+       return movieService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable int id) {
+        return movieService.getById(id);
+    }
+
+    @PostMapping("/")
+    public Movie createMovie(@RequestBody Movie movie) {
+        movieService.save(movie);
+
+        return movie;
+    }
+
+    @PutMapping("/")
+    public Movie updateMovie(@RequestBody Movie movie) {
+        movieService.save(movie);
+
+        return movie;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteMovieById(@PathVariable int id) {
+        movieService.deleteById(id);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
